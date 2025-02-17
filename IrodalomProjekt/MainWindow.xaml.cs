87236c2826ebc2a5612b8c3b2c161a5e60e98159
TekulicsB <tekulicsb@kkszki.hu>
2025-02-17 09:22:27 +0100

    Betöltés, Kérdésfeltöltés kidolgozva

--- IrodalomProjekt/MainWindow.xaml.cs
+++ IrodalomProjekt/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using IrodalomProjekt.Models;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,14 +9,19 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Linq;
+using System.IO;
+using System.Linq.Expressions;
+using Microsoft.Win32;
+
 
 namespace IrodalomProjekt
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        private static List<Kerdes> kerdesek = new List<Kerdes>();
+        private static int aktualisIndex = 0;
         public MainWindow()
         {
             InitializeComponent();
@@ -23,12 +29,48 @@ namespace IrodalomProjekt
 
         private static void KerdeseketFeltolt(string fileName)
         {
+            kerdesek.Clear();
+            try
+            {
+                StreamReader sr = new StreamReader(fileName, Encoding.UTF8);
+                while (!sr.EndOfStream)
+                {
+                    string kerdesSzovege = sr.ReadLine();
+                    string valaszA = sr.ReadLine();
+                    string valaszB = sr.ReadLine();
+                    string valaszC = sr.ReadLine();
+                    string[] utolsoSor = sr.ReadLine().Split(' ');
+                    string helyesValasz = utolsoSor[2];
+                        
+                }
 
+            }
+            catch (Exception ex)
+            {
+            }
         }
 
         private void BetoltesClick(object sender, RoutedEventArgs e)
         {
-
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "TXT Fájlok (*.txt)|*.txt";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    KerdeseketFeltolt(openFileDialog.FileName);
+                    MessageBox.Show("Sikeres Betöltés!", "Információ", MessageBoxButton.OK, MessageBoxImage.Information);
+                    if (kerdesek.Count > 0)
+                    {
+                        aktualisIndex = 0;
+                        MutatKerdes(aktualisIndex);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
 
         private void KiertekelesClick(object sender, RoutedEventArgs e)
